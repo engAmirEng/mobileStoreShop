198657d31943e97117b5ca6ad3875d943c004600
Amir <eng.amir.bu@gmail.com>
2021-04-29 19:19:19 +0430

    Register models into admin.py.
    -*changing the type of screenSize field to decimal
    -*adding one record to db

--- phones/admin.py
+++ phones/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Mobiles, Manufacturer, Brand
 
-# Register your models here.
+
+@admin.register(Mobiles)
+class MobilesAdmin(admin.ModelAdmin):
+    list_display = [i.name for i in Mobiles._meta.get_fields()]
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["name", "nationality"]
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
--- phones/models.py
+++ phones/models.py
@@ -4,18 +4,28 @@ from django.db import models
 class Manufacturer(models.Model):
     country_name = models.CharField(max_length=50, primary_key=True)
 
+    def __str__(self):
+        return self.country_name
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
     nationality = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Mobiles(models.Model):
     model_name = models.CharField(max_length=50, primary_key=True)
     color = models.CharField(max_length=50)
     price = models.PositiveIntegerField()
-    screenSize = models.PositiveIntegerField()
+    screenSize = models.DecimalField(max_digits=4, decimal_places=2)
     inStock = models.BooleanField()
     manufacturer = models.ForeignKey(
         "Manufacturer", on_delete=models.DO_NOTHING)
     brand = models.ForeignKey("Brand", on_delete=models.CASCADE)
+    # brand_nationality = models.ForeignKey("")
+
+    def __str__(self):
+        return self.model_name
