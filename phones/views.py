60f9f29ee9775b4dda26c0ac08248bc1fc77223e
Amir <eng.amir.bu@gmail.com>
2021-05-02 14:09:03 +0430

    Requered report feature(get native mobiles) added
    get the mobiles that have same brand and manufacturer nationality whit raw "SQL query"

--- phones/views.py
+++ phones/views.py
@@ -1,3 +1,6 @@
+from django.http import JsonResponse
+from django.core import serializers
+from .forms import BrandNatReportForm, BrandMobReportForm, NatManForm
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.utils import timezone
@@ -67,20 +70,22 @@ def update_record(request):
         massage = ""
         try:
             get_object_or_404(Mobiles, pk=request.POST["model_name_update"])
-            updating_obj = Mobiles.objects.filter(model_name=request.POST["model_name_update"])
-            initial = {"model_name": updating_obj[0].model_name, "color": updating_obj[0].color, 
-                    "price": updating_obj[0].price, "screen_size": updating_obj[0].screen_size, 
-                    "in_stock": updating_obj[0].in_stock, "manufacturer": updating_obj[0].manufacturer, 
-                    "brand": updating_obj[0].brand}
+            updating_obj = Mobiles.objects.filter(
+                model_name=request.POST["model_name_update"])
+            initial = {"model_name": updating_obj[0].model_name, "color": updating_obj[0].color,
+                       "price": updating_obj[0].price, "screen_size": updating_obj[0].screen_size,
+                       "in_stock": updating_obj[0].in_stock, "manufacturer": updating_obj[0].manufacturer,
+                       "brand": updating_obj[0].brand}
             filled_form = MobileForm(initial=initial)
         except:
             massage = "این گوشی ثبت نشده است"
-        return render(request, "templates/phones/update_record.html", \
-                                            {"status": status, "filled_form": filled_form, "massage":massage})
+        return render(request, "templates/phones/update_record.html",
+                      {"status": status, "filled_form": filled_form, "massage": massage})
     else:
         status = ""
         try:
-            updating_obj = get_object_or_404(Mobiles, pk=request.POST["model_name"])
+            updating_obj = get_object_or_404(
+                Mobiles, pk=request.POST["model_name"])
             updated_form = MobileForm(request.POST, instance=updating_obj)
             if updated_form.is_valid():
                 updated_form.save()
@@ -90,20 +95,17 @@ def update_record(request):
         # updating_obj = Mobiles.objects.get(pk=request.POST["model_name"])
         except:
             massage = "مدل گوشی قابل تغییر نمی باشد"
-        return render(request, "templates/phones/update_record.html", \
-                                        {"status": status, "massage":massage})
+        return render(request, "templates/phones/update_record.html",
+                      {"status": status, "massage": massage})
 
-
-from .forms import BrandNatReportForm, BrandMobReportForm, NatManForm
-from django.core import serializers
-from django.http import JsonResponse
 
 def report(request):
     if request.method == "GET":
         brand_nat_form = BrandNatReportForm()
         brand_mob_form = BrandMobReportForm()
         nat_man_form = NatManForm()
-        form = {"brand_nat_form":brand_nat_form, "brand_mob_form":brand_mob_form, "nat_man_form":nat_man_form}
+        form = {"brand_nat_form": brand_nat_form,
+                "brand_mob_form": brand_mob_form, "nat_man_form": nat_man_form}
         return render(request, "templates/phones/reports.html", form)
     else:
         if 'brand_nationality' in request.POST and 'manufacturer' not in request.POST:
@@ -121,10 +123,16 @@ def report(request):
         elif 'brand_nationality' and 'manufacturer' in request.POST:
             brand_nationality = request.POST["brand_nationality"]
             manufacturer = request.POST["manufacturer"]
-            obj = Mobiles.objects.filter(brand__nationality=brand_nationality, manufacturer=manufacturer)
+            obj = Mobiles.objects.filter(
+                brand__nationality=brand_nationality, manufacturer=manufacturer)
             obj_json = serializers.serialize("json", obj)
             data = {"Mobiles": obj_json}
             return JsonResponse(data)
-        else:
-            return HttpResponse()
-
+        elif "get_native" in request.POST:
+            sql_query = "SELECT model_name, color, in_stock, brand_id, manufacturer_id, screen_size, \
+                nationality as brand_nationality FROM (SELECT * FROM phones_mobiles LEFT JOIN phones_brand on \
+                    phones_mobiles.brand_id = phones_brand.name) WHERE nationality = manufacturer_id"
+            obj = Mobiles.objects.raw(sql_query)
+            obj_json = serializers.serialize("json", obj)
+            data = {"Mobiles": obj_json}
+            return JsonResponse(data)
